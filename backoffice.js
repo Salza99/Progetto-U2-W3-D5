6ddcdaf7e38eda50c38cb41581744a0d27fa29d8
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-08-25 16:59:38 +0200

    complete remove button, start put button

--- backoffice.js
+++ backoffice.js
@@ -26,64 +26,70 @@ const creaProdotto = async () => {
   });
 };
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const getProduct = await fetch(URL, {
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE4OWMwMzRmZjAwMTQwM2Y1MzgiLCJpYXQiOjE2OTI5NTA5MjEsImV4cCI6MTY5NDE2MDUyMX0.bpCEkrdfG0n-89OwzBbwGaJ08Iz-dckSDvCItcx-uV0",
-      "content-type": "application/JSON",
-    },
-  });
-  let listId = 1;
-  const resp = await getProduct.json();
-  let ul = document.getElementById("lista-prodotti");
+// window.addEventListener("DOMContentLoaded", async () => {
 
-  console.log(resp);
-  for (let i = 0; i < resp.length; i++) {
-    let li = document.createElement("li");
-    ul.innerHTML += `<li id="product ${listId}" class= " list-group-item">Id: ${resp[i]._id}, Nome prodotto: ${
-      resp[i].name
-    }, Prezzo: ${resp[i].price}</li><button onclick="paginaModificaProdotto(${
-      (resp[i].name, resp[i].description, resp[i].brand, resp[i].imageUrl, resp[i].price)
-    })"   class="btn btn-secondary ">Modifica</button><button  id="btn-delete" class="btn btn-danger mb-4">Rimuovi</button>`;
-    listId++;
-  }
-
-  console.log(resp);
-});
-
-button.onclick = creaProdotto;
-
-const paginaModificaProdotto = (previusName, previusDesc, previusBrand, previusImage, previusPrice) => {
-  let nameInput = document.getElementById("name").value;
-  let descrizioneInput = document.getElementById("descrizione").value;
-  let BrandInput = document.getElementById("brand").value;
-  let ImageInput = document.getElementById("image").value;
-  let priceInput = document.getElementById("prezzo").value;
-  nameInput = previusName;
-  descrizioneInput = previusDesc;
-  BrandInput = previusBrand;
-  ImageInput = previusImage;
-  priceInput = previusPrice;
-};
-// let putProduct = await fetch(URL + listid, {
-//     method: "PUT",
-//     body: JSON.stringify({
-//       id: listid,
-//       name: nameInput,
-//       ddescription: descrizioneInput,
-//       brand: BrandInput,
-//       imageUrl: ImageInput,
-//       price: priceInput,
-//     }),
+//   const getProduct = await fetch(URL, {
 //     headers: {
 //       Authorization:
 //         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE4OWMwMzRmZjAwMTQwM2Y1MzgiLCJpYXQiOjE2OTI5NTA5MjEsImV4cCI6MTY5NDE2MDUyMX0.bpCEkrdfG0n-89OwzBbwGaJ08Iz-dckSDvCItcx-uV0",
 //       "content-type": "application/JSON",
 //     },
 //   });
+//   let listId = 1;
+//   const resp = await getProduct.json();
+//   let ul = document.getElementById("lista-prodotti");
+
+//   console.log(resp);
+//   for (let i = 0; i < resp.length; i++) {
+//     let li = document.createElement("li");
+//     ul.innerHTML += `<li id="product ${listId}" class= " list-group-item">Id: ${resp[i]._id}, Nome prodotto: ${
+//       resp[i].name
+//     }, Prezzo: ${resp[i].price}</li><button onclick="paginaModificaProdotto(${
+//       (resp[i].name, resp[i].description, resp[i].brand, resp[i].imageUrl, resp[i].price)
+//     })"   class="btn btn-secondary ">Modifica</button><button  id="btn-delete" class="btn btn-danger mb-4">Rimuovi</button>`;
+//     listId++;
+//   }
+
+//   console.log(resp);
+// });
+
+button.onclick = creaProdotto;
+
+window.addEventListener("DOMContentLoaded", async () => {
+  let id = new URLSearchParams(window.location.search);
+  let id2 = id.get("id");
+  console.log(id2);
+
+  if (id2) {
+    let obj = await fetch(URL + id2, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE4OWMwMzRmZjAwMTQwM2Y1MzgiLCJpYXQiOjE2OTI5NTA5MjEsImV4cCI6MTY5NDE2MDUyMX0.bpCEkrdfG0n-89OwzBbwGaJ08Iz-dckSDvCItcx-uV0",
+        "content-type": "application/JSON",
+      },
+    });
+    let result = await obj.json();
+    console.log(result);
+  }
+});
+// let putProduct = await fetch(URL + listid, {
+//   method: "PUT",
+//   body: JSON.stringify({
+//     id: listid,
+//     name: nameInput,
+//     ddescription: descrizioneInput,
+//     brand: BrandInput,
+//     imageUrl: ImageInput,
+//     price: priceInput,
+//   }),
+//   headers: {
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE4OWMwMzRmZjAwMTQwM2Y1MzgiLCJpYXQiOjE2OTI5NTA5MjEsImV4cCI6MTY5NDE2MDUyMX0.bpCEkrdfG0n-89OwzBbwGaJ08Iz-dckSDvCItcx-uV0",
+//     "content-type": "application/JSON",
+//   },
+// });
 // let newName = document.getElementById("name").value;
-//   let newDescription = document.getElementById("descrizione").value;
-//   let newBrand = document.getElementById("brand").value;
-//   let newImage = document.getElementById("image").value;
-//   let newPrice = document.getElementById("prezzo").value;
+// let newDescription = document.getElementById("descrizione").value;
+// let newBrand = document.getElementById("brand").value;
+// let newImage = document.getElementById("image").value;
+// let newPrice = document.getElementById("prezzo").value;
--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const URL = "https://striveschool-api.herokuapp.com/api/product/";
 
 window.addEventListener("DOMContentLoaded", async () => {
+  creaPagina();
+});
+
+const eliminaProdotto = async (id) => {
+  console.log(id);
+  //   let idProd = resp._id;
+  let elimina = await fetch(URL + id, {
+    method: "DELETE",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjE4OWMwMzRmZjAwMTQwM2Y1MzgiLCJpYXQiOjE2OTI5NTA5MjEsImV4cCI6MTY5NDE2MDUyMX0.bpCEkrdfG0n-89OwzBbwGaJ08Iz-dckSDvCItcx-uV0",
+      "content-type": "application/JSON",
+    },
+  }).then((res) => creaPagina()); // or res.json()
+};
+const creaPagina = async () => {
   const getProduct = await fetch(URL, {
     headers: {
       Authorization:
@@ -9,19 +25,27 @@ window.addEventListener("DOMContentLoaded", async () => {
     },
   });
   const resp = await getProduct.json();
-  console.log(resp);
+  console.log(resp[1]._id);
   let main = document.getElementById("main");
+  main.innerHTML = "";
   for (let i = 0; i < resp.length; i++) {
+    let respString = JSON.stringify(resp[i]._id);
+    console.log(respString);
+
     main.innerHTML += `
-    
-    <div class="card col-4 mb-3 ">
-      <img src="${resp[i].imageUrl}" class="card-img-top">
-      <div class="card-body">
-        <h5 class="card-title">${resp[i].name}</h5>
-        <p class="card-text">${resp[i].description}</p>
-        <p class="card-text"><small class="text-muted">${resp[i].price}€</small></p>
-        <button class="btn btn-primary">Modifica</button>
-      </div>
-    `;
+        
+        <div class="card col-4 mb-3 ">
+          <img src="${resp[i].imageUrl}" class="card-img-top">
+          <div id="corpo" class="card-body">
+            <h5 class="card-title">${resp[i].name}</h5>
+            <p class="card-text">${resp[i].description}</p>
+            <p class="card-text"><small class="text-muted">${resp[i].price}€</small></p>
+            <button onclick="modificaProdotto('${resp[i]._id}')" class="btn btn-primary">Modifica</button>
+            <button onclick="eliminaProdotto('${resp[i]._id}')" id="btn-remove${i}" class="btn btn-danger">Rimuovi</button>
+          </div>
+        `;
   }
-});
+};
+const modificaProdotto = async (id) => {
+  window.location.href = "./backoffice.html?id=" + id;
+};
